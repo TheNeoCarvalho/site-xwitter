f2f409a92e72e8e7450994be928b43214ebff54a
Manoel Carvalho <manoelmecat@gmail.com>
2024-10-23 11:04:33 -0300

    feat: add redirect

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -42,13 +42,13 @@ class AuthController extends Controller
   public function login()
   {
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-      $email = $_POST['email'];
+      $username = $_POST['username'];
       $password = $_POST['password'];
 
       $db = Database::connect();
-      $stm = $db->prepare("SELECT * FROM users WHERE email = :email");
+      $stm = $db->prepare("SELECT * FROM users WHERE username = :username");
 
-      $stm->bindParam(":email", $email);
+      $stm->bindParam(":username", $username);
       $stm->execute();
 
       $user = $stm->fetch();
--- app/views/auth/login.php
+++ app/views/auth/login.php
@@ -13,8 +13,8 @@
     <h2 class="text-2xl font-semibold text-center text-gray-700 mb-6">Login</h2>
     <form action="/login" method="POST">
       <div class="mb-4">
-        <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-        <input type="text" id="email" name="email" required
+        <label for="username" class="block text-sm font-medium text-gray-700">Usuário</label>
+        <input type="text" id="username" name="username" required
           class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
       </div>
       <div class="mb-6">
